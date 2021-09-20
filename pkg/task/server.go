215669e14db926c2b132686b333ebd2d62869922
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-09-20 22:57:03 +0200

    5: Define Client/Server API using interfaces

--- pkg/task/server.go
+++ pkg/task/server.go
@@ -16,62 +16,79 @@ package task
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"io/ioutil"
 	"net"
 
 	"github.com/apex/log"
+	"github.com/pkg/errors"
 	"github.com/szaffarano/gotas/pkg/config"
 )
 
-type Client struct {
+// Server is the taskd server
+type Server interface {
+	// NextClient returns a client
+	NextClient() (Client, error)
+	Close() error
+}
+
+// Client represents a Taskd client connected to taskd server
+type Client interface {
+	Read(buf []byte) (int, error)
+	Write(buf []byte) (int, error)
+	Close() error
+}
+
+func (s *server) Close() error {
+	return s.listener.Close()
+}
+
+type client struct {
 	conn net.Conn
 }
 
-func (c *Client) Read(buf []byte) (int, error) {
+func (c *client) Read(buf []byte) (int, error) {
 	return c.conn.Read(buf)
 }
 
-func (c *Client) Write(buf []byte) (int, error) {
+func (c *client) Write(buf []byte) (int, error) {
 	return c.conn.Write(buf)
 }
 
-func (c *Client) Close() error {
+func (c *client) Close() error {
 	return c.conn.Close()
 }
 
-type Server struct {
+type server struct {
 	listener net.Listener
 }
 
-func (s *Server) NextClient() (*Client, error) {
+func (s *server) NextClient() (Client, error) {
 	conn, err := s.listener.Accept()
 	if err != nil {
 		return nil, err
 	}
-	return &Client{conn}, nil
+
+	return &client{conn}, nil
 }
 
-func (s *Server) Close() {
-	s.listener.Close()
-}
-
-func NewServer() (*Server, error) {
+func NewServer() (Server, error) {
 	var ca []byte
 	var cert tls.Certificate
 	var err error
 	conf := config.Get()
 
 	if ca, err = ioutil.ReadFile(conf.Ca.Cert); err != nil {
-		log.Fatalf("Failed to read root CA file", err)
+		return nil, errors.Wrap(err, "Failed to read root CA file")
 	}
 
 	roots := x509.NewCertPool()
 	if ok := roots.AppendCertsFromPEM(ca); !ok {
-		log.Fatal("failed to parse root certificate")
+		return nil, fmt.Errorf("failed to parse root certificate")
 	}
 
 	if cert, err = tls.LoadX509KeyPair(conf.Server.Cert, conf.Server.Key); err != nil {
-		log.Fatalf("Error reading certificate file", err)
+		return nil, errors.Wrap(err, "Error reading certificate file")
 	}
 
 	cfg := &tls.Config{
@@ -86,5 +103,5 @@ func NewServer() (*Server, error) {
 	}
 
 	log.Infof("Listening on %s...", conf.Server.BindAddress)
-	return &Server{listener}, nil
+	return &server{listener}, nil
 }
