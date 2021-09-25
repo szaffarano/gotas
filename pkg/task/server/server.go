b0b329288870fe94821230104b89fb1225f1eede
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-09-25 16:48:50 +0200

    3: Dummy implementation of server command

--- pkg/task/server/server.go
+++ pkg/task/server/server.go
@@ -1,5 +1,17 @@
 package server
 
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net"
+
+	"github.com/apex/log"
+	"github.com/szaffarano/gotas/pkg/config"
+	"github.com/szaffarano/gotas/pkg/task/repo"
+)
+
 // Server is the taskd server
 type Server interface {
 	// NextClient returns a client
@@ -14,4 +26,70 @@ type Client interface {
 	Read(buf []byte) (int, error)
 	Write(buf []byte) (int, error)
 	Close() error
+}
+
+func (s *server) Close() error {
+	return s.listener.Close()
+}
+
+type client struct {
+	conn net.Conn
+}
+
+func (c *client) Read(buf []byte) (int, error) {
+	return c.conn.Read(buf)
+}
+
+func (c *client) Write(buf []byte) (int, error) {
+	return c.conn.Write(buf)
+}
+
+func (c *client) Close() error {
+	return c.conn.Close()
+}
+
+type server struct {
+	listener net.Listener
+}
+
+func (s *server) NextClient() (Client, error) {
+	conn, err := s.listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	return &client{conn}, nil
+}
+
+func NewServer(cfg config.Config) (Server, error) {
+	var ca []byte
+	var cert tls.Certificate
+	var err error
+
+	if ca, err = ioutil.ReadFile(cfg.Get(repo.CaCert)); err != nil {
+		return nil, fmt.Errorf("reading root CA file: %v", err)
+	}
+
+	roots := x509.NewCertPool()
+	if ok := roots.AppendCertsFromPEM(ca); !ok {
+		return nil, fmt.Errorf("reading creating root CA pool: %v", err)
+	}
+
+	if cert, err = tls.LoadX509KeyPair(cfg.Get(repo.ServerCert), cfg.Get(repo.ServerKey)); err != nil {
+		return nil, fmt.Errorf("reading certificate file: %v", err)
+	}
+
+	tlsCfg := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    roots,
+	}
+
+	listener, err := tls.Listen("tcp", cfg.Get(repo.BindAddress), tlsCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Infof("Listening on %s...", cfg.Get(repo.BindAddress))
+	return &server{listener}, nil
 }
--- pkg/task/server/server_test.go
+++ pkg/task/server/server_test.go
@@ -1,8 +1,80 @@
 package server
 
 import (
+	"fmt"
+	"net"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/szaffarano/gotas/pkg/config"
+	"github.com/szaffarano/gotas/pkg/task/repo"
 )
 
 func TestServer(t *testing.T) {
+	t.Run("server with valid config", func(t *testing.T) {
+		cfg, err := config.Load(filepath.Join("testdata", "repo_one", "config"))
+		assert.Nil(t, err)
+
+		before := nextFreePort(t, 1025)
+
+		cfg.Set(repo.BindAddress, fmt.Sprintf("localhost:%d", before))
+
+		srv, err := NewServer(cfg)
+		assert.Nil(t, err)
+		assert.NotNil(t, srv)
+
+		assert.Less(t, before, nextFreePort(t, 1025))
+		assert.NoError(t, srv.Close())
+	})
+
+	t.Run("server with invalid key cert", func(t *testing.T) {
+		cfg, err := config.Load(filepath.Join("testdata", "repo_invalid_config", "config"))
+		assert.Nil(t, err)
+
+		srv, err := NewServer(cfg)
+		assert.NotNil(t, err)
+		assert.Nil(t, srv)
+	})
+
+	t.Run("server with malformed ca cert", func(t *testing.T) {
+		cfg, err := config.Load(filepath.Join("testdata", "repo_invalid_config", "config"))
+		assert.Nil(t, err)
+
+		cfg.Set(repo.CaCert, filepath.Join("testdata", "repo_invalid_config", "certs", "ca.cert-invalid.pem"))
+
+		_, err = NewServer(cfg)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("server with invalid ca cert", func(t *testing.T) {
+		cfg, err := config.Load(filepath.Join("testdata", "repo_invalid_config", "config"))
+		assert.Nil(t, err)
+
+		cfg.Set(repo.CaCert, "invalid")
+
+		_, err = NewServer(cfg)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("server with invalid bind address", func(t *testing.T) {
+		cfg, err := config.Load(filepath.Join("testdata", "repo_one", "config"))
+		assert.Nil(t, err)
+
+		cfg.Set(repo.BindAddress, "1:2:3:localhost")
+
+		_, err = NewServer(cfg)
+		assert.NotNil(t, err)
+	})
+}
+
+func nextFreePort(t *testing.T, initial int) int {
+	for ; initial < 65535; initial++ {
+		if l, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", initial)); err == nil {
+			defer assert.NoError(t, l.Close())
+			return initial
+		}
+	}
+	t.Error("Not available port found")
+	return 0
 }
